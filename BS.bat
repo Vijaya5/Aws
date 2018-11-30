531768a64b2043b6fc4c2011716c806a170b414c
Vijaya5 <doddapaneni.vijji@gmail.com>
2018-11-29 22:07:24 -0600

    update BS.bat

--- BS.bat
+++ BS.bat
@@ -1,7 +1,2 @@
-click on create a new job
-choose FreeStyle project
-Enter a description for the job and configure how many old jobs should be retained
-configure how the source code can be retrieved.If you for example using GIT, enter the url to the GIT repository.If the repository is not public, you may need to configure the credentials
-set Build triggers option like when builds needs to be run
-Finally save 
-click on build now
+echo "running batch file"
+echo "Done running batch file"
