61a4b9bf6d783485bbbf310642ecc00f3f8252d7
Vijaya5 <doddapaneni.vijji@gmail.com>
2018-11-29 23:30:16 -0600

    Update a.bat

--- a.bat
+++ a.bat
@@ -1,3 +1,3 @@
-xcopy "C:\Program Files (x86)\Jenkins\workspace" "E:\Destination1"
+xcopy "C:\Program Files (x86)\Jenkins" "E:\Destination1"
 cd "C:\Program Files (x86)\Jenkins\workspace\CopyFiles"
 CALL b.bat
