613540804198d325820652629b5d94775f46d5f0
Vijaya5 <doddapaneni.vijji@gmail.com>
2018-11-30 22:35:59 -0600

    modify javadoc

--- b.bat
+++ b.bat
@@ -1,5 +1,5 @@
 echo "Running JavaDoc on Git Jenkins integration job..."
 cd "E:\Destination1"
 dir /s /b *.java >file.lst
-javadoc -d E:\_javadoc @file.lst
+javadoc:javadoc -d E:\_javadoc @file.lst
 echo "Done Git Jenkins integration job..."
