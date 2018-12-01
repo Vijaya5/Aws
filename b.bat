8cdbe2f2d6926ca6ad8ab8d9c1a088e53be8deed
Vijaya5 <doddapaneni.vijji@gmail.com>
2018-11-30 22:32:38 -0600

    removed java location

--- b.bat
+++ b.bat
@@ -1,5 +1,5 @@
 echo "Running JavaDoc on Git Jenkins integration job..."
 cd "E:\Destination1"
 dir /s /b *.java >file.lst
-cd "C:\Program Files\Java\jdk1.8.0_191\bin" javadoc -d E:\_javadoc @file.lst
+javadoc -d E:\_javadoc @file.lst
 echo "Done Git Jenkins integration job..."
