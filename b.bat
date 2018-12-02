b5d9821cc133ed47852a64903ece087c8d5d91b0
Vijaya5 <doddapaneni.vijji@gmail.com>
2018-12-01 22:12:26 -0600

    Update b.bat

--- b.bat
+++ b.bat
@@ -1,5 +1,5 @@
 echo "Running JavaDoc on Git Jenkins integration job..."
 c:
 cd "C:\Program Files\Java\jdk1.8.0_191\bin" 
-javadoc -d E:\_javadoc E:\Destination1\HelloWorld.java"
+javadoc -d E:\_javadoc E:\Destination1\HelloWorld.java
 echo "Done Git Jenkins integration job..."
